5f62d7d6d448ed66d1c0d196704aa3e333c41218
Vyoman Jain <75789232+VyoJ@users.noreply.github.com>
2024-11-19 18:46:09 +0530

    Removed Kafka from client

--- v3/server/consumer.py
+++ v3/server/consumer.py
@@ -10,7 +10,17 @@ consumer = KafkaConsumer(
     value_deserializer=lambda x: json.loads(x.decode("utf-8")),
 )
 
-es = Elasticsearch(["elasticsearch_host:9200"])
+es = Elasticsearch(["http://localhost:9200"])
+
+# for message in consumer:
+#     es.index(index="logs", document=message.value)
 
 for message in consumer:
+    print(f"Message: {message.value}")
     es.index(index="logs", document=message.value)
+
+    response = es.search(
+        index="logs",
+        body={"query": {"match": {"message_type": message.value.get("message_type")}}},
+    )
+    print(f"Indexed message: {response['hits']['hits']}")
