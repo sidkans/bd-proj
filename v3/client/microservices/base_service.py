11a47f663df1eb4ef4956a71a769eb1b558b1a7d
vishva7 <vishvamehta301@gmail.com>
2024-11-19 15:25:42 +0530

    Removed Kafka from base

--- v3/client/microservices/base_service.py
+++ v3/client/microservices/base_service.py
@@ -2,19 +2,16 @@ import threading
 import time
 from uuid import uuid4
 from datetime import datetime
-from kafka import KafkaProducer
 import json
 from schema import RegistrationMessage, HeartbeatMessage
+from fluent import sender
 
 
 class BaseService:
     def __init__(self, service_name: str):
         self.service_name = service_name
         self.node_id = f"{service_name}-{str(uuid4())}"
-        self.kafka_producer = KafkaProducer(
-            bootstrap_servers=["localhost:9092"],
-            value_serializer=lambda v: json.dumps(v).encode("utf-8"),
-        )
+        self.logger = sender.FluentSender(service_name.lower(), host='localhost', port=24224)
         self._register_service()
         self._start_heartbeat()
 
@@ -27,7 +24,7 @@ class BaseService:
             "timestamp": datetime.utcnow().isoformat(),
         }
         # Send registration message to Kafka
-        self.kafka_producer.send("service_registry", self.registration_message)
+        self.logger.emit('registration', self.registration_message)
 
     def _send_heartbeat(self):
         while True:
@@ -36,7 +33,7 @@ class BaseService:
                 "node_id": self.node_id,
                 "timestamp": datetime.utcnow().isoformat(),
             }
-            self.kafka_producer.send("heartbeats", heartbeat)
+            self.logger.emit('heartbeat', heartbeat)
             time.sleep(10)
 
     def _start_heartbeat(self):
